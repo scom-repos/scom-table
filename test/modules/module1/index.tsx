b3a545e4d19c67f21b7e1088b57806067472a63f
nigellai2020 <65589812+nigellai2020@users.noreply.github.com>
2023-05-15 18:57:01 +0800

    Update API endpoint

--- test/modules/module1/index.tsx
+++ test/modules/module1/index.tsx
@@ -16,7 +16,7 @@ export default class Module1 extends Module {
             <i-scom-table
                 margin={{ left: 'auto', right: 'auto' }}
                 data={{
-                    apiEndpoint: 'https://api.dune.com/api/v1/query/2030664/results?api_key=324WhvsCHWCji2pkgtfa0JDqDu8j0FdD',
+                    apiEndpoint: "/dune/query/2030664",
                     options: {
                         title: 'Ethereum Beacon Chain Deposits Entity',
                         columns: [
